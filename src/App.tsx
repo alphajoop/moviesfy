6669e14ddaa801840aace0b8fc0f20f092dfb6c5
Alpha DIOP <alphakhoss@gmail.com>
2024-05-15 19:13:07 +0000

    🎉::tada

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container">
+      <h1 className="md:text-5xl font-semibold text-4xl text-slate-950">Movie App</h1>
+    </div>
+  );
+}
+
+export default App;
