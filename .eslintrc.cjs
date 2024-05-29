2fc894d32931e7e88dfc176d9e1416de2c88d429
Alpha DIOP <alphakhoss@gmail.com>
2024-05-29 13:34:22 +0000

    🔧:: update configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,12 +8,6 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
